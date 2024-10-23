014e8c7074ba34b42ae9441a0dbd5bc9125e9d75
Iker <411999@tecnicatura.frc.utn.edu.ar>
2024-10-23 01:17:55 -0300

    problema 2.11

---
+++ Problema-2.11/Baraja.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2._11
+{
+    public class Baraja
+    {
+        Carta[] cartas = new Carta[40];
+        public Baraja()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                if(i <10)
+                {
+                    cartas[i] = new Carta(i, "Espada");
+                }
+                else if(i>10 && i <20)
+                {
+                    for(int j = 1; j < 10; j++)
+                    {
+                        cartas[i] = new Carta(j, "Basto");
+                    }
+                }
+                else if(i>20 && i <30)
+                {
+                    for (int j = 1; j < 10; j++)
+                    {
+                        cartas[i] = new Carta(j, "Oro");
+                    }
+                }
+                else if( i>30 && i <40)
+                {
+                    for (int j = 1; j < 10; j++)
+                    {
+                        cartas[i] = new Carta(j, "Oro");
+                    }
+                }
+            }
+        }
+
+        public void Barajar()
+        {
+            Random rnd = new Random();
+            for(int i = 0;i < 40; i++)
+            {
+                cartas[i]= 
+            }
+        }
+    }
+}
---
+++ Problema-2.11/Carta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Problema_2._11
+{
+    public class Carta
+    {
+        public int Numero { get; set; }
+        public string Palo { get; set; }
+
+        public Carta(int numero, string palo)
+        {
+            if (numero >= 1 && numero <= 12 && numero != 8 && numero != 9)
+            {
+                Console.WriteLine("El numero no debe ser menor a 1 o mayor a 12 o igual a 8 o 9");
+                Numero = numero;
+            }
+            else if (numero == 8)
+            {
+                Numero = 10;
+            }
+            else if (numero == 9)
+            {
+                Numero = 11;
+            }
+            else if (numero == 10)
+            {
+                Numero = 12;
+            }
+            if (palo == "Espada" || palo == "Basto" || palo == "Oro" || palo == "Copas")
+            {
+                Palo = palo;
+            }
+            else
+            {
+                Console.WriteLine("Palo No Disponible");
+                Palo = string.Empty;
+            }
+        }
+    }
+}
